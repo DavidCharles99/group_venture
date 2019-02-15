5ecb48f9478df6b48f225edda5fbc33bae526d2f
David <charlesinvestingltd@gmail.com>
2019-02-15 13:46:06 -0500

    message

--- lib/group_venture/cli.rb
+++ lib/group_venture/cli.rb
@@ -9,21 +9,23 @@ class GroupVenture::CLI
 
   def top_icos
     puts "top ICO's"
-    @coin = GroupVenture::Coin.scrape_coins
-    @coin.each.with_index(1) do |input|
-      puts "#{input} #{@coin.name}"
+    @coins = GroupVenture::Coin.scrape_coins
+    @coins.each do |coin|
+      @coin_mc = coin.mc
+      puts "#{coin.name}  #{coin.price}"
     end
   end
+
 
   def menu
     input = nil
     while input != "exit"
-      puts "enter the name of the coin for its YLD curve or type exit:"
+      puts "enter the name of the coin for more information or type exit:"
       input = gets.strip.downcase
       case input
 
       when "bitcoin"
-        puts "[1mnth: 12%] [2mnth: 18%] [3mnth: 12%] [6mnth: 6%] [1yr: 2%]"
+        puts "#{@coin_mc}"
         puts "type y/n to buy"
         usin = gets.strip.downcase
         if usin == "y"
@@ -33,7 +35,17 @@ class GroupVenture::CLI
         end
 
       when "ether"
-        puts "[1mnth: 14%] [2mnth: 8%] [3mnth: 16%] [6mnth: 26%] [1yr: 12%]"
+        puts "#{@coin_mc}"
+        puts "type y/n to buy"
+        usin = gets.strip.downcase
+        if usin == "y"
+          puts "hey ya!"
+        else
+          top_icos
+        end
+
+      when "xrp"
+        puts "#{@coin_mc}"
         puts "type y/n to buy"
         usin = gets.strip.downcase
         if usin == "y"
@@ -52,5 +64,4 @@ class GroupVenture::CLI
    def good_bye
      puts "seeya!"
    end
-
-end
+ end
--- lib/group_venture/coin.rb
+++ lib/group_venture/coin.rb
@@ -1,10 +1,13 @@
 class GroupVenture::Coin
-  attr_accessor :name, :price, :v, :mc, :url
+  attr_accessor :name, :price, :mc, :url
 
   def self.scrape_coins
     coins = []
     coins << self.scrape_bitcoin
-    #coins << self.scrape_ether
+    coins << self.scrape_ether
+    coins << self.scrape_xrp
+
+    coins
 
 
   end
@@ -15,8 +18,28 @@ class GroupVenture::Coin
     coin = self.new
     coin.name =  doc.search("h1.details-panel-item--name").text
     coin.price = doc.search("span.h2.text-semi-bold.details-panel-item--price__value").text
-    coin.mc = doc.search("div.coin-summary-item-detail").first.text.strip
+    coin.mc = doc.search("table.table.cmc-table-striped.cmc-cc-summary-table").text
     coin
-    
   end
+
+  def self.scrape_ether
+    url = "https://coinmarketcap.com/currencies/ethereum/"
+    doc = Nokogiri::HTML(open(url))
+    coin = self.new
+    coin.name =  doc.search("h1.details-panel-item--name").text
+    coin.price = doc.search("span.h2.text-semi-bold.details-panel-item--price__value").text
+    coin.mc = doc.search("table.table.cmc-table-striped.cmc-cc-summary-table").text
+    coin
+  end
+
+  def self.scrape_xrp
+    url = "https://coinmarketcap.com/currencies/ripple/"
+    doc = Nokogiri::HTML(open(url))
+    coin = self.new
+    coin.name =  doc.search("h1.details-panel-item--name").text
+    coin.price = doc.search("span.h2.text-semi-bold.details-panel-item--price__value").text
+    coin.mc = doc.search("table.table.cmc-table-striped.cmc-cc-summary-table").text
+    coin
+  end
+
 end
