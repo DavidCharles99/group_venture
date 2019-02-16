c1333444cef6d06e4ebbf07fdeb5313016302bb8
David <charlesinvestingltd@gmail.com>
2019-02-15 23:18:59 -0500

    buy feature works now so does wallet feature

--- lib/group_venture/cli.rb
+++ lib/group_venture/cli.rb
@@ -1,6 +1,9 @@
 #controller
 class GroupVenture::CLI
-
+  attr_accessor :counter1, :counter2, :counter3
+  @@counter1 = 0
+  @@counter2 = 0
+  @@counter3 = 0
   def call
     top_icos
     menu
@@ -25,37 +28,42 @@ class GroupVenture::CLI
       case input
 
       when "bitcoin"
-        puts "#{@coin_mc}"
+        puts "#{GroupVenture::Coin.scrape_coins[0].mc}"
         puts "type y/n to buy"
         usin = gets.strip.downcase
         if usin == "y"
-          puts "hey ya!"
+        @@counter1 += 1
+        puts "you have #{@@counter1} bitcoin"
         else
           top_icos
         end
 
       when "ether"
-        puts "#{@coin_mc}"
+        puts "#{GroupVenture::Coin.scrape_coins[1].mc}"
         puts "type y/n to buy"
         usin = gets.strip.downcase
         if usin == "y"
-          puts "hey ya!"
+          @@counter2 += 1
+          puts "you have #{@@counter2} ethereum"
         else
           top_icos
         end
 
       when "xrp"
-        puts "#{@coin_mc}"
+        puts "#{GroupVenture::Coin.scrape_coins[2].mc}"
         puts "type y/n to buy"
         usin = gets.strip.downcase
         if usin == "y"
-          puts "hey ya!"
+          @@counter3 += 1
+          puts "you have #{@@counter3} ripple"
         else
           top_icos
         end
 
-      when "list"
-        top_icos
+      when "wallet"
+        puts "bitcoin wallet =  [#{@@counter1}]"
+        puts "ethereum wallet = [#{@@counter2}]"
+        puts "ripple wallet =   [#{@@counter3}]"
       end
 
      end
